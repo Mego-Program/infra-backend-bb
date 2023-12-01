14c2967a341e4951e397c13b24d7e9d02fd85692
barak.noam.haviv <barak.software.solutions@gmail.com>
2023-12-01 09:21:49 +0200

    update files fro deploy

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-})
+if(!process.env.IS_VERCEL){
+  server.listen(port, () => {
+      console.log(`Server running on port: ${port}`);
+  })
+}
+
+export default app; 
