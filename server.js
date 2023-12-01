acbba9529d13cbade8a412b626f2cd0460bc6df6
barak.noam.haviv <barak.software.solutions@gmail.com>
2023-12-01 09:44:26 +0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import app from "./app.js";
+import app from "./index.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,8 +8,6 @@ const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
-if(!process.env.IS_VERCEL){
-  server.listen(port, () => {
-      console.log(`Server running on port: ${port}`);
-  })
-}
+server.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+})
